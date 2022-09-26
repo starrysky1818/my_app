b5629b319301680846c545ca8c1a3379926ec850
demil <demili1031@gmail.com>
2022-09-26 21:37:38 +1000

    1. Account page options created (near to finalisation)

--- lib/Page/Profile/accountPage.dart
+++ lib/Page/Profile/accountPage.dart
@@ -5,8 +5,13 @@ class MyAccount extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Body(),
-
+      body: Container(
+          margin: const EdgeInsets.all(10),
+          padding: new EdgeInsets.fromLTRB(20, 40.0, 20.0, 40.0),
+          child:
+            Body(),
+      ),
+      backgroundColor: const Color.fromRGBO(10,39,59,1),
     );
   }
 }
---
+++ lib/Page/Profile/profile_pic.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class ProfilePic extends StatelessWidget {
+  const ProfilePic({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 115,
+      width: 115,
+      child: Stack(
+        fit: StackFit.expand,
+        clipBehavior: Clip.none,
+        children: [
+          CircleAvatar(
+            backgroundImage: AssetImage('images/Default_Profile_Photo.jpg'),
+          ),
+          Positioned(
+            right: -15,
+            bottom: 0,
+            child: SizedBox(
+              height: 46,
+              width: 46,
+              child: ClipOval(
+                child: Material(
+                  color: Colors.white30,
+                  child: InkWell(
+                    splashColor: Colors.black,
+                    onTap: () {},
+                    child: Icon(Icons.camera_alt_rounded),
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
