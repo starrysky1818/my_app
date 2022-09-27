63473b1af28b2ddd2694d3577b3129a7fc59a290
Yinghan <yinghan.feng@uqconnect.edu.au>
2022-09-27 15:03:13 +1000

    Setting almost finished. But still need to add some links for another pages that didn't finish. So just used print instead of link.

--- lib/Page/timeSetting.dart
+++ lib/Page/timeSetting.dart
@@ -46,7 +46,7 @@ class MyTimeSetting extends StatelessWidget {
           ),
           Container(
               width: screenWidth,
-              height: screenWidth * 0.15,
+              height: screenWidth * 0.4,
               margin: const EdgeInsets.all(10),
               padding: new EdgeInsets.fromLTRB(20, 10.0, 20.0, 10.0),
               decoration: BoxDecoration(
@@ -54,7 +54,7 @@ class MyTimeSetting extends StatelessWidget {
                   border: Border.all(color: Color.fromRGBO(39,69,92,1)),
                   borderRadius: BorderRadius.all(Radius.circular(100))
               ),
-              child: MyApp(
+              child: MyClockPage(
               )
           ),
         ],
@@ -64,203 +64,150 @@ class MyTimeSetting extends StatelessWidget {
   }
 }
 
-// class MyClockPage extends StatefulWidget {
+class MyClockPage extends StatefulWidget {
+  @override
+  State<MyClockPage> createState() => _MyClockPageState();
+}
+
+class _MyClockPageState extends State<MyClockPage> {
+
+  // creating text ediiting controller to take hour
+  // and minute as input
+  TextEditingController hourController = TextEditingController();
+  TextEditingController minuteController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+          child: Column(children: <Widget>[
+            SizedBox(height: 30),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  height: 40,
+                  width: 60,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.rectangle,
+                      color: Colors.yellow,
+                      borderRadius: BorderRadius.circular(11)),
+                  child: Center(
+                    child: TextField(
+                      controller: hourController,
+                      keyboardType: TextInputType.number,
+                    ),
+                  ),
+                ),
+                SizedBox(width: 20),
+                Container(
+                  height: 40,
+                  width: 60,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.rectangle,
+                      color: Colors.yellow,
+                      borderRadius: BorderRadius.circular(11)),
+                  child: Center(
+                    child: TextField(
+                      controller: minuteController,
+                      keyboardType: TextInputType.number,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              margin: const EdgeInsets.all(25),
+              child: TextButton(
+                child: const Text(
+                  'Create alarm',
+                  style: TextStyle(fontSize: 20.0),
+                ),
+                onPressed: () {
+                  int hour;
+                  int minutes;
+                  hour = int.parse(hourController.text);
+                  minutes = int.parse(minuteController.text);
+
+                  // creating alarm after converting hour
+                  // and minute into integer
+                  FlutterAlarmClock.createAlarm(hour, minutes);
+                },
+              ),
+            ),
+            // ElevatedButton(
+            //   onPressed: () {
+            //     // show alarm
+            //     FlutterAlarmClock.showAlarms();
+            //   },
+            //   child: const Text(
+            //     'Show Alarms',
+            //     style: TextStyle(fontSize: 20.0),
+            //   ),
+            // ),
+          ]));
+  }
+}
+
+// class MyApp extends StatefulWidget {
+//   const MyApp({Key? key}) : super(key: key);
+//
 //   @override
-//   State<MyClockPage> createState() => _MyClockPageState();
+//   State<MyApp> createState() => _MyAppState();
 // }
+// class _MyAppState extends State<MyApp> {
+//   @override
+//   void initState() {
+//     super.initState();
+//   }
 //
-// class _MyClockPageState extends State<MyClockPage> {
-//
-//   // creating text ediiting controller to take hour
-//   // and minute as input
-//   TextEditingController hourController = TextEditingController();
-//   TextEditingController minuteController = TextEditingController();
 //   @override
 //   Widget build(BuildContext context) {
-//     return Scaffold(
-//       // appBar: AppBar(
-//       //   title: const Text('Done'),
-//       //   centerTitle: true,
-//       // ),
-//       body: Center(
-//           child: Column(children: <Widget>[
-//             SizedBox(height: 30),
-//             Row(
-//               mainAxisAlignment: MainAxisAlignment.center,
-//               children: [
-//                 Container(
-//                   height: 40,
-//                   width: 60,
-//                   decoration: BoxDecoration(
-//                       shape: BoxShape.rectangle,
-//                       color: Colors.yellow,
-//                       borderRadius: BorderRadius.circular(11)),
-//                   child: Center(
-//                     child: TextField(
-//                       controller: hourController,
-//                       keyboardType: TextInputType.number,
-//                     ),
-//                   ),
-//                 ),
-//                 SizedBox(width: 20),
-//                 Container(
-//                   height: 40,
-//                   width: 60,
-//                   decoration: BoxDecoration(
-//                       shape: BoxShape.rectangle,
-//                       color: Colors.yellow,
-//                       borderRadius: BorderRadius.circular(11)),
-//                   child: Center(
-//                     child: TextField(
-//                       controller: minuteController,
-//                       keyboardType: TextInputType.number,
-//                     ),
-//                   ),
-//                 ),
-//               ],
-//             ),
-//             Container(
-//               margin: const EdgeInsets.all(25),
-//               child: TextButton(
-//                 child: const Text(
-//                   'Create alarm',
-//                   style: TextStyle(fontSize: 20.0),
-//                 ),
-//                 onPressed: () {
-//                   int hour;
-//                   int minutes;
-//                   hour = int.parse(hourController.text);
-//                   minutes = int.parse(minuteController.text);
+//     int value1 = 0;
+//     int value2 = 0;
+//     return Center(
+//             child: Column(children: <Widget>[
+//               Container(
+//                 margin: const EdgeInsets.all(25),
+//                   child: Row (
+//                         children: <Widget>[
+//                           SizedBox(width: 5),
+//                       TextField(
+//                         obscureText: true,
+//                           decoration: InputDecoration(
+//                             border: OutlineInputBorder(),
+//                             labelText: 'hi',
+//                           ),
+//                           // clipBehavior: Clip.none,
+//                           cursorColor: Colors.cyan,
+//                           onChanged: (value) {
+//                             value1 = int.parse(value);
+//                             print("The value entered is : $value");
+//                           }
+//                           ),
+//                           SizedBox(width: 5),
 //
-//                   // creating alarm after converting hour
-//                   // and minute into integer
-//                   FlutterAlarmClock.createAlarm(hour, minutes);
-//                 },
-//               ),
-//             ),
-//             ElevatedButton(
-//               onPressed: () {
-//
-//                 // show alarm
-//                 FlutterAlarmClock.showAlarms();
-//               },
-//               child: const Text(
-//                 'Show Alarms',
-//                 style: TextStyle(fontSize: 20.0),
-//               ),
-//             ),
-//             Container(
-//               margin: const EdgeInsets.all(25),
-//               child: TextButton(
+//                        TextField(
+//                               onChanged: (value) {
+//                                 value2 = int.parse(value);
+//                                 print("The value entered is : $value");
+//                               }
+//                          ),
+//                           SizedBox(width: 5),
+//                     ]),
+//                 ),
+//               Container(
+//                 margin: const EdgeInsets.all(25),
+//                 child: TextButton(
 //                   child: const Text(
-//                     'Create timer',
+//                     'Show alarms',
 //                     style: TextStyle(fontSize: 20.0),
 //                   ),
 //                   onPressed: () {
-//                     int minutes;
-//                     minutes = int.parse(minuteController.text);
-//
-//                     // create timer
-//                     FlutterAlarmClock.createTimer(minutes);
-//                     showDialog(
-//                         context: context,
-//                         builder: (context) {
-//                           return AboutDialog(
-//                             children: [
-//                               Center(
-//                                 child: Text("Timer is set",
-//                                     style: TextStyle(
-//                                         fontSize: 20, fontWeight: FontWeight.bold)),
-//                               )
-//                             ],
-//                           );
-//                         });
-//                   }),
-//             ),
-//             ElevatedButton(
-//               onPressed: () {
-//
-//                 // show timers
-//                 FlutterAlarmClock.showTimers();
-//               },
-//               child: Text(
-//                 "Show Timers",
-//                 style: TextStyle(fontSize: 17),
+//                     FlutterAlarmClock.showAlarms();
+//                   },
+//                 ),
 //               ),
+//             ]
 //             )
-//           ])),
-//     );
+//       );
 //   }
 // }
-
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-class _MyAppState extends State<MyApp> {
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Center(
-            child: Column(children: <Widget>[
-              Container(
-                margin: const EdgeInsets.all(25),
-                child: TextButton(
-                  child: const Text(
-                    'Create alarm at 23:59',
-                    style: TextStyle(fontSize: 20.0),
-                  ),
-                  onPressed: () {
-                    FlutterAlarmClock.createAlarm(23, 59);
-                  },
-                ),
-              ),
-              Container(
-                margin: const EdgeInsets.all(25),
-                child: TextButton(
-                  child: const Text(
-                    'Show alarms',
-                    style: TextStyle(fontSize: 20.0),
-                  ),
-                  onPressed: () {
-                    FlutterAlarmClock.showAlarms();
-                  },
-                ),
-              ),
-              Container(
-                margin: const EdgeInsets.all(25),
-                child: TextButton(
-                  child: const Text(
-                    'Create timer for 42 seconds',
-                    style: TextStyle(fontSize: 20.0),
-                  ),
-                  onPressed: () {
-                    FlutterAlarmClock.createTimer(42);
-                  },
-                ),
-              ),
-              Container(
-                margin: const EdgeInsets.all(25),
-                child: TextButton(
-                  child: const Text(
-                    'Show Timers',
-                    style: TextStyle(fontSize: 20.0),
-                  ),
-                  onPressed: () {
-                    FlutterAlarmClock.showTimers();
-                  },
-                ),
-              ),
-            ])),
-      ),
-    );
-  }
-}
